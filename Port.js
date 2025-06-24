09ba1acf7fa6f56b29360e2172953bc7d4c84ec2
unknown <ELVICTOR1>
2025-06-24 19:24:28 -0300

    big bang

---
+++ Port.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+  const hmtl = document.documentElement;
+  hmtl.classList.toggle("light");
+
+  const img = document.querySelector(".profile-photo");
+
+  if (hmtl.classList.contains("light")) {
+    img.setAttribute("src", "../img/avatar2.jpeg");
+  } else {
+    img.setAttribute("src", "../img/avatar1.jpeg");
+  }
+}
