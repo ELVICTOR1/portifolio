d274f89203b7829f44fcf54646148c09345454f2
ELVICTOR1 <vihxavier10@gmail.com>
2025-06-24 19:51:15 -0300

    Update Port.js

--- Port.js
+++ Port.js
@@ -5,8 +5,8 @@ function toggleMode() {
   const img = document.querySelector(".profile-photo");
 
   if (hmtl.classList.contains("light")) {
-    img.setAttribute("src", "../img/avatar2.jpeg");
+    img.setAttribute("src", "./img/avatar2.jpeg");
   } else {
-    img.setAttribute("src", "../img/avatar1.jpeg");
+    img.setAttribute("src", "./img/avatar1.jpeg");
   }
 }
